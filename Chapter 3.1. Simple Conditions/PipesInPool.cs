0bef67fa10ba8e117fa20d5c773d50cbc9d59691
OVER-Studio <3714693+over-studio@users.noreply.github.com>
2019-08-23 15:30:04 -0400

    Inputs are taken from the console.

--- Chapter 3.1. Simple Conditions/PipesInPool.cs
+++ Chapter 3.1. Simple Conditions/PipesInPool.cs
@@ -7,24 +7,25 @@ class MainClass
 		//Console.WriteLine ("Pipes in pool");
 
 		//Console.Write("Volume of the pool = ");
-		//int poolVolume = int.Parse(Console.ReadLine());
-		int poolVolume = 1000;
+		int poolVolume = int.Parse(Console.ReadLine());
+		//int poolVolume = 1000;
 
 		//Console.Write("Flow rate of the first pipe per hour = ");
-		//int rate1 = int.Parse(Console.ReadLine());
-		int rate1 = 100;
+		int rate1 = int.Parse(Console.ReadLine());
+		//int rate1 = 100;
 
 		//Console.Write("Flow rate of the second pipe per hour = ");
-		//int rate2 = int.Parse(Console.ReadLine());
-		int rate2 = 120;
+		int rate2 = int.Parse(Console.ReadLine());
+		//int rate2 = 120;
 
 		//Console.Write("Absence hours = ");
-		//int absence = int.Parse(Console.ReadLine());
-		double absence = 3;
+		double absence = int.Parse(Console.ReadLine());
+		//double absence = 3;
 
 		double totalWater1 = rate1 * absence;
 		double totalWater2 = rate2 * absence;
 		double totalWater = totalWater1 + totalWater2;
+		
 		if(totalWater <= poolVolume)
 		{
 			double percent1 = Math.Truncate(totalWater1 * 100 / totalWater);
